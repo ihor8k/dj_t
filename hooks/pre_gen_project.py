b8ebccf8bdad3f747bc77f3bf688eef1997f2801
Kypeshchuk Ihor <ihor8k@gmail.com>
2020-07-09 10:31:28 +0300

    fix string format

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -6,6 +6,6 @@ SUCCESS = '\x1b[1;32m [SUCCESS]: '
 
 project_slug = '{{ cookiecutter.project_slug }}'
 if hasattr(project_slug, 'isidentifier'):
-    assert project_slug.isidentifier(), ''{}' project slug is not a valid Python identifier.'.format(project_slug)
+    assert project_slug.isidentifier(), f'{project_slug} project slug is not a valid Python identifier.'
 
-assert project_slug == project_slug.lower(), ''{}' project slug should be all lowercase'.format(project_slug)
+assert project_slug == project_slug.lower(), f'{project_slug} project slug should be all lowercase'
